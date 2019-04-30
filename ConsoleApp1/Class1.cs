4a4db02e56ce6f93a51eca463c39578215a6fae2
Zalak Pandya <zalakpatel393@gmail.com>
2019-04-30 09:51:49 -0600

    third commit

---
+++ ConsoleApp1/Class1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp1
+{
+    class Class1
+    {
+        public static void newfunction()
+        {
+
+        }
+    }
+}
